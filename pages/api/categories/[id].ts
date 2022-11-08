276121afedf81da9689d9af5b42555f3aab4a6d3
Omer Gatenio <gateniomer@gmail.com>
2022-11-08 18:32:53 +0200

    added error message to categories endpoint

--- pages/api/categories/[id].ts
+++ pages/api/categories/[id].ts
@@ -4,7 +4,9 @@ import {CATEGORIES} from '../../../data/categories';
 export default function handler(req:NextApiRequest,res:NextApiResponse) {
   const {id} = req.query;
   const category = getCategory(id);
-  res.status(200).json(category);
+  category ? 
+  res.status(200).json(category):
+  res.status(400).json({error:"category not found"});
 }
 
 export const getCategory = (id:any) => {
