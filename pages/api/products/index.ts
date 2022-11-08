c4f478bf66e9cc0874dc2e3aa9dfa9732959cc67
Omer Gatenio <gateniomer@gmail.com>
2022-11-08 17:03:22 +0200

    updated api

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -5,6 +5,7 @@ export default function handler(req:NextApiRequest,res:NextApiResponse) {
   const {search} = req.query;
   if (search) res.status(200).json(searchProducts(search));
   res.status(200).json(products);
+  res.status(500).json({error:'error'});
 }
 
 export const searchProducts = (input:any) => {
