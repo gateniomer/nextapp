eb66638dce55b528b2764b5a850be67a08690392
gateniomer <gateniomer@gmail.com>
2022-11-17 19:31:19 +0200

    updated checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -2,12 +2,16 @@ import { useRouter } from "next/router";
 import { useAppSelector } from "../utils/hooks";
 import {useEffect,useState} from 'react';
 import styles from '../styles/Checkout.module.css';
+import Image from "next/image";
 
 const Checkout = ()=>{
   const cartProducts = useAppSelector(state=>state.userDetails.cart);
   const router = useRouter();
   const user = useAppSelector(state=>state.userDetails.user);
   const [loading,setLoading] = useState(false);
+
+  const total = cartProducts.reduce((acc,product)=>product.price*product.quantity+acc,0);
+  
   
   //redirect user if not signed-in
   // useEffect(()=>{
@@ -33,22 +37,32 @@ const Checkout = ()=>{
   return (
     <div className={styles.container}>
     <h2>Proceed to Payment</h2>
-    <p>Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.</p>
+    
     <div className={styles.detailsContainer}>
       <div className={styles.checkoutItemsContainer}>
       {
         cartProducts.map((product,index)=>{
           return <div key={index} className={styles.checkoutItem}>
-            
-            <span>{product.title} x{product.quantity}</span>
-            <span>Size: {product.size}</span>
-            <span>Price: {product.price} x {product.quantity} = {product.price*product.quantity}₪</span>
+            <Image src={product.image} width='90px' height='100px' objectFit="cover" />
+            <div>
+              <span>{product.title} x{product.quantity}</span>
+              <span>Size: {product.size}</span>
+              <span>Price: {product.price} x {product.quantity} = {product.price*product.quantity}₪</span>
+            </div>
           </div>
         })
       }
       </div>
-      <div>
-        {user && <button className="btn" onClick={onClickHandler}>{loading? 'Loading...' : 'Buy Now'}</button>}
+      <div className={styles.stripeContainer}>
+        <h3>Order Summary</h3>
+        <span>Items: {total}₪</span>
+        <span>Shipping & handling: 0₪</span>
+        <span className={styles.totalPrice}>Order Total: <strong>{total}₪</strong></span>
+        {user && <button className={`btn ${styles.btnStripe}`} onClick={onClickHandler}>{loading? 'Loading...' : 'Pay with Stripe'}</button>}
+        <p>Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.</p>
+        <div className={styles.stripeBudgetContainer}>
+          <Image src={'/stripe.svg'} layout={'fill'} objectFit={'contain'}/>
+        </div>
       </div>
     </div>
     </div>)
