1f08a43a0493f6b60484424df17247fda06a7431
gateniomer <gateniomer@gmail.com>
2022-11-28 22:56:41 +0200

    edit home page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,16 @@
 import styles from '../styles/Footer.module.css'
-
+import {faGithub,faLinkedin} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      Created by Omer Gatenio
+      <ul>
+        <li><a href="https://github.com/gateniomer" aria-label="Github" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}/></a></li>
+        <li><a href="www.linkedin.com/in/omer-gatenio-949482174" aria-label="Linkedin" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin}/></a></li>
+        <li><a href="mailto:gateniomer@gmail.com" aria-label="Gmail" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope}/></a></li>
+      </ul>
+      <span>Desgined & Created by Omer Gatenio | A learning project for Web development.</span>
     </div>
   )
 }
