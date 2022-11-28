bc20403c16fe173536bc294a10abad4bc8d6019d
gateniomer <gateniomer@gmail.com>
2022-11-28 22:58:10 +0200

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className={styles.footer}>
       <ul>
         <li><a href="https://github.com/gateniomer" aria-label="Github" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faGithub}/></a></li>
-        <li><a href="www.linkedin.com/in/omer-gatenio-949482174" aria-label="Linkedin" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin}/></a></li>
+        <li><a href="https://linkedin.com/in/omer-gatenio-949482174" aria-label="Linkedin" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin}/></a></li>
         <li><a href="mailto:gateniomer@gmail.com" aria-label="Gmail" target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faEnvelope}/></a></li>
       </ul>
       <span>Desgined & Created by Omer Gatenio | A learning project for Web development.</span>
